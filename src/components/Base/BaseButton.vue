88e87b24e201bd7883e384fc36f0e97d17359619
Miguel Santos <miguel.santos@tripleplay.tv>
2020-10-24 15:58:02 +0100

    Creating a base button component

---
+++ src/components/Base/BaseButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="cursor-pointer">
+        <a :href="urlLocation">{{ text }}</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'BaseButton',
+        props: {
+            urlLocation: {
+                type: String,
+                default: 'javascript:void(0);',
+            },
+            text: {
+                type: String,
+                default: ''
+            }
+        },
+    };
+</script>
