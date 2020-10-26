58ae21cfbeb6adfd01b8954c85045fccdaa130e6
Miguel Santos <miguel.santos@tripleplay.tv>
2020-10-26 15:50:43 +0000

    Adding arrow button functionality to scroll down to stats

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,20 +1,24 @@
 <template>
-    <div class="flex relative">
+    <div class="flex relative" ref="hero">
         <div class="flex items-center text-white relative">
             <base-svg class="flex" source="hero"></base-svg>
             <div class="flex flex-col items-start absolute w-full
                     px-10 sm:px-12 md:px-20 lg:px-32 xl:p-hero">
-                <div class="flex font-condensed font-bold text-highlightxs sm:text-highlightsm
+                <div class="flex font-condensed font-bold leading-tight text-highlightxs sm:text-highlightsm
                         md:text-highlightmd lg:text-highlightlg xl:text-highlightxl">
                     {{ title }}
                 </div>
-                <div class="text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl">
+                <div class="leading-tight text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl">
                     {{ description }}
                 </div>
             </div>
         </div>
-        <div class="absolute w-full flex-1 bottom-0 self-end m-auto mb-4">
-            <base-svg class="text-customgray b-1 self-center m-auto" svg-class="w-16" source="arrow-down">
+        <div class="absolute w-full flex-1 bottom-0 self-end m-auto mb-0 md:mb-1 lg:mb-2 xl:mb-4 cursor-pointer"
+             @click="() => scrollToStats()"
+        >
+            <base-svg class="text-customgray b-1 self-center m-auto hover:text-white"
+                      svg-class="w-4 sm:w-6 lg:w-8 xl:w-10"
+                      source="arrow-down">
             </base-svg>
         </div>
     </div>
@@ -36,6 +40,14 @@
                 default: ''
             }
         },
-        components: { BaseSvg }
+        components: { BaseSvg },
+        methods: {
+            scrollToStats() {
+                const heroBoundingRect = this.$refs.hero.getBoundingClientRect();
+                scrollTo({
+                    top: heroBoundingRect.height,
+                });
+            }
+        }
     };
 </script>
