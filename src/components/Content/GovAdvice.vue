33b7aa751026a8d3b08933c970ab8f291c4a8080
Miguel Santos <miguel.santos@tripleplay.tv>
2020-10-29 17:05:26 +0000

    Adjusting the width of GovAdvice in lg devices

--- src/components/Content/GovAdvice.vue
+++ src/components/Content/GovAdvice.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="flex items-center justify-between text-center text-base lg:text-lg xl:text-xl p-8
-                flex-col lg:flex-row lg:p-govadvice">
+                flex-col lg:flex-row lg:p-24 xl:p-govadvice">
         <div v-for="(advice, index) in govAdvice" :key="index"
-             class="flex flex-col mx-0 my-4 lg:m-6 xl:m-govadvice last:m-0">
+             class="flex flex-col mx-0 my-4 lg:my-0 lg:mx-6 xl:m-govadvice w-2/6 xl:last:m-0">
             <div
                 class="font-condensed font-bold tracking-tight
                        mb-1 lg:mb-2 text-xl lg:text-2xl xl:text-4xl">
